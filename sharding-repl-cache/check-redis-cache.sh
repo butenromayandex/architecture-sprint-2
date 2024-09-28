70b6e1f970e7d7ec7265f86af38b1bc7dd7b0a21
r.butenko <!@#$%^&*()_Fndhpsydc8>
2024-09-28 12:50:17 +0300

    Спринт 2
    Внесены некоторые правки

--- sharding-repl-cache/check-redis-cache.sh
+++ sharding-repl-cache/check-redis-cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'Время запроза (кеширования)'
-curl -o /dev/null -s -w 'Total Time: %{time_total}s\n' 'http://localhost:8080/helloDoc/users'
+echo 'Время запроза (без кеширования)'
+curl -o /dev/null -s -w 'Время: %{time_total}s\n' 'http://localhost:8080/helloDoc/users'
 
 echo 'Время запроса (с кешиированием)'
-curl -o /dev/null -s -w 'Total Time: %{time_total}s\n' 'http://localhost:8080/helloDoc/users'
+curl -o /dev/null -s -w 'Время: %{time_total}s\n' 'http://localhost:8080/helloDoc/users'
--- sharding-repl-cache/init.sh
+++ sharding-repl-cache/init.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 echo 'Start initiating mongo cluster'
-docker compose up -d --force-recreate
+docker compose up -d
 
-sleep 5
-
+sleep 10
 echo 'Start configure configSrv'
 docker exec -it configSrv mongosh --port 27017 --shell /configSrv-init.js
 
+sleep 5
 echo 'Start configure shard1 and replicas'
 docker exec -it shard1 mongosh --port 27030 --shell /shard1-repl-init.js
 
+sleep 5
 echo 'Start configure shard2 and replicas'
 docker exec -it shard2 mongosh --port 27040 --shell /shard2-repl-init.js
 
